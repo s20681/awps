1164e3e5e7934fd45265b0a50b4650feb3e7c1ce
mzapl <maciej.zakrzewski@zohomail.eu>
2021-11-23 20:52:04 +0100

    fiddling with profiles

---
+++ src/main/java/com/pjatk/awps/AppConfig.java
@@ -0,0 +1,14 @@
+package com.pjatk.awps;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Profile("development")
+@PropertySource("application-development.properties")
+public class AppConfig {
+    public static void main(String[] args) {
+        System.setProperty("spring.config.name", "application-development.properties");
+    }
+}
--- src/main/java/com/pjatk/awps/DatabaseConfig.java
+++ src/main/java/com/pjatk/awps/DatabaseConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
 
 @Configuration
+@Profile("production")
 public class DatabaseConfig {
 
     @Value("${spring.datasource.url}")
